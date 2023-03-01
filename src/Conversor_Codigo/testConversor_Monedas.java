5e7acf0c91920aefd490de8c1eb1b350fe218d9a
Luis_Angel9 <luisangelsotob9@gmail.com>
2023-03-01 10:01:16 -0400

    subir repositorio

--- src/Conversor_Codigo/testConversor_Monedas.java
+++ src/Conversor_Codigo/testConversor_Monedas.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class testConversor_Monedas {
     
     public static void main(String[] args) {
-
+      new inicio_panel().setVisible(true);
        
        
    
